114877b6e6ea537cee82475d817e17536d6621a7
Alex Romantsov <alekseyromantsov@gmail.com>
2023-01-15 18:08:37 +0600

    For checking

--- yatube_api/api/permissions.py
+++ yatube_api/api/permissions.py
@@ -11,7 +11,6 @@ class IsAuthorOrReadOnlyPermission(permissions.BasePermission):
         )
 
     def has_object_permission(self, request, view, obj):
-        return (
-                obj.author == request.user
+        return (obj.author == request.user
                 or request.method in permissions.SAFE_METHODS
-        )
+                )
--- yatube_api/api/views.py
+++ yatube_api/api/views.py
@@ -4,7 +4,7 @@ from rest_framework.pagination import LimitOffsetPagination
 from rest_framework.permissions import (IsAuthenticatedOrReadOnly,
                                         IsAuthenticated)
 
-from posts.models import Comment, Follow, Group, Post
+from posts.models import Comment, Group, Post
 
 from .permissions import IsAuthorOrReadOnlyPermission
 from .serializers import (PostSerializer, GroupSerializer, CommentSerializer,
