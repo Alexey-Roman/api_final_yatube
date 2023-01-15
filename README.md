1c4a351cb951863c94b4807b522bca4e4f966aa7
Alex Romantsov <alekseyromantsov@gmail.com>
2023-01-15 14:49:10 +0600

    add posts\models.py

--- README.md
+++ README.md
@@ -1,2 +1,58 @@
-# api_final
-api final
+# API для Yatube - спринт №9
+## Описание
+
+
+## Технологии
+* Python 3.8
+* Django 3.2
+* SQLite3
+* Django REST Framework (DRF)
+* Django ORM
+
+
+### Как запустить проект:
+
+Клонировать репозиторий и перейти в него в командной строке:
+
+```
+git clone https://github.com/Alexey-Roman/api_final_yatube.git
+```
+
+```
+cd api_final_yatube
+```
+
+Cоздать и активировать виртуальное окружение:
+
+```bash
+python -m venv env
+```
+
+```
+source env/bin/activate
+```
+
+```bash
+python -m pip install --upgrade pip
+```
+
+Установить зависимости из файла requirements.txt:
+
+```bash
+pip install -r requirements.txt
+```
+
+Выполнить миграции:
+
+```bash
+python yatube_api\manage.py migrate
+```
+
+Запустить проект:
+
+```bash
+python yatube_api\manage.py runserver
+```
+
+### Документация для API Yatube
+http://127.0.0.1:8000/redoc/
--- yatube_api/posts/models.py
+++ yatube_api/posts/models.py
@@ -4,23 +4,89 @@ from django.db import models
 User = get_user_model()
 
 
+class Group(models.Model):
+    """Модель для Группы"""
+
+    title = models.CharField(
+        max_length=200,
+        help_text='Название группы'
+    )
+    slug = models.SlugField(
+        unique=True,
+        help_text='URL группы'
+    )
+    description = models.TextField(help_text='Описание группы')
+
+    def __str__(self):
+        return self.title
+
+
 class Post(models.Model):
-    text = models.TextField()
-    pub_date = models.DateTimeField('Дата публикации', auto_now_add=True)
+    """Модель для Поста"""
+    text = models.TextField(help_text='Текст публикации')
+    pub_date = models.DateTimeField(
+        verbose_name='Дата публикации',
+        auto_now_add=True
+    )
     author = models.ForeignKey(
-        User, on_delete=models.CASCADE, related_name='posts')
+        User,
+        on_delete=models.CASCADE,
+        related_name='posts'
+    )
     image = models.ImageField(
-        upload_to='posts/', null=True, blank=True)
+        upload_to='posts/',
+        null=True, blank=True
+    )
+    group = models.ForeignKey(
+        Group,
+        on_delete=models.SET_NULL,
+        related_name="posts",
+        blank=True,
+        null=True
+    )
 
     def __str__(self):
         return self.text
 
 
 class Comment(models.Model):
+    """Модель для Комментариев"""
     author = models.ForeignKey(
-        User, on_delete=models.CASCADE, related_name='comments')
+        User,
+        on_delete=models.CASCADE,
+        related_name='comments'
+    )
     post = models.ForeignKey(
-        Post, on_delete=models.CASCADE, related_name='comments')
-    text = models.TextField()
+        Post,
+        on_delete=models.CASCADE,
+        related_name='comments'
+    )
+    text = models.TextField(verbose_name='Текст комментария')
     created = models.DateTimeField(
-        'Дата добавления', auto_now_add=True, db_index=True)
+        verbose_name='Дата добавления',
+        auto_now_add=True,
+        db_index=True
+    )
+
+
+class Follow(models.Model):
+    """Модель для Подписчиков"""
+
+    user = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='follower'
+    )
+    following = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='following'
+    )
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(
+                fields=['user', 'following'],
+                name='unique_following'
+            )
+        ]
